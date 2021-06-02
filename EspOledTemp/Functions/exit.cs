a56ce018420c26e59b1efa5c13457a51443ddf06
Patrik Hermansson <patrik@paheco.nu>
2021-06-02 15:31:36 +0200

    Menus done

---
+++ EspOledTemp/Functions/exit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EspOledTemp.Functions
+{
+    public class exit
+    {
+        public void mnuExitClicked()
+        {
+            DialogResult res = MessageBox.Show("Are you sure you want to exit", "Really exit?", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (res == DialogResult.OK)
+            {
+                if (Application.MessageLoop)
+                    Application.Exit();
+                else
+                    Environment.Exit(1);
+            }
+        }
+    }
+}
