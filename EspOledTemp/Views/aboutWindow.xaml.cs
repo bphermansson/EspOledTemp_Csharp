47183a8fe98524926f5700c26d41630fccd7fd18
Patrik Hermansson <patrik@paheco.nu>
2021-10-04 23:12:45 +0200

    Finished the About window.

--- EspOledTemp/Views/aboutWindow.xaml.cs
+++ EspOledTemp/Views/aboutWindow.xaml.cs
@@ -9,15 +9,27 @@ namespace EspOledTemp
         public aboutWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
         }
 
         private void aboutClicked(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             Close();
         }
-        private void Hyperlink_RequestNavigateAbout(object sender, RequestNavigateEventArgs e)
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
         {
             System.Diagnostics.Process.Start(e.Uri.ToString());
+        }
+        public string InfoText
+        {
+            get { return string.Format(
+                "The base for this project is a NodeMCU,\n a Soc based on ESP8266. It has a sensor for temperature and" +
+                " humidity connected, a HTU21D module.\nIt also have an OLED connected for showing measured values," +
+                "these values are also sent via Mqtt to a Mqtt broker. The values are also presented via a dynamic webpage " +
+                "where you can see the values in near realtime." +
+                "This program is a Windows app that receives the Mqtt - values and display them in a window."
+            ); }
         }
     }
 }
